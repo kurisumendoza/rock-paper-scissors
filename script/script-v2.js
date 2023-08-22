34a383c4c083830eae80a09915e6f3d3a803702b
kurisu26 <creative.kristopher@gmail.com>
2023-08-22 23:03:16 +0800

    Copy commit ab91e58 to script-v2.js

--- script/script-v2.js
+++ script/script-v2.js
@@ -1,1 +1,37 @@
-console.log('hello');
+const choices = ['rock', 'paper', 'scissors'];
+
+let computerSelection = '';
+
+let playerSelection = prompt('Choose one: Rock, Paper, or Scissors');
+playerSelection = playerSelection.toLowerCase();
+
+function getComputerChoice() {
+  computerSelection = choices[Math.floor(Math.random() * choices.length)];
+  return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      return 'You Lose! Paper beats Rock.';
+    } else if (computerSelection === 'scissors') {
+      return 'You Win! Rock beats Scissors.';
+    } else return 'Tie. Please try again.';
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      return 'You Win! Paper beats Rock.';
+    } else if (computerSelection === 'scissors') {
+      return 'You Lose! Scissors beat Paper.';
+    } else return 'Tie. Please try again.';
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
+      return 'You Lose! Rock beats Scissors.';
+    } else if (computerSelection === 'paper') {
+      return 'You win! Scissors beat Paper.';
+    } else return 'Tie. Please try again.';
+  } else return 'Sorry. Your choice is invalid. Please try again.'
+}
+
+console.log('You: ' + playerSelection);
+console.log('Computer: ' + getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
